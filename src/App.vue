65cee9da9e06a255e13e2598fc7cd4dd52ad5e3a
hplar003 <ralphmaronilla003@gmail.com>
2020-01-11 09:45:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layouts/Header";
+import Axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todos,
+    AddTodo,
+    Header
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      //this.todos = this.todos.filter(todo => todo.id !== id);
+      Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => {
+          this.todos = this.todos.filter(todo => todo.id !== id);
+          return res;
+        })
+        .catch(error => {
+          return Promise.reject(error);
+        });
+    },
+    addTodo(newTodo) {
+      // deconstruct the newTodo object
+      const { title, completed } = newTodo;
+      //this.todos = [...this.todos, newTodo];
+      Axios.post("https://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed
+      })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(error => {
+          return Promise.reject(error);
+        });
+    }
+  },
+  created() {
+    Axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(res => (this.todos = res.data))
+      .catch(error => {
+        return Promise.reject(error);
+      });
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+.btn {
+  display: inline;
+  border: none;
+  background: rgb(29, 180, 218);
+  color: #333;
+  padding: 7px 20px;
+  font-size: 1rem;
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 1px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: rgb(24, 146, 177);
+  color: rgb(240, 236, 236);
 }
 </style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <h1>Todo List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: teal;
+  color: #ffffff;
+  text-align: center;
+  padding: 10px;
+  margin-bottom: 30px;
+}
+</style>
